0768299eff1f49193c7454c0db32d89ca35da3af
dalanienatsch <dienatsch93@gmail.com>
2020-05-09 13:54:32 -0600

    Added show messages and create new message

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender = "Dalan Ienatsch";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSendMessage() {
+  var newMessage = new Message("4", this.subject.nativeElement.value, this.msgText.nativeElement.value, this.currentSender);
+  this.addMessageEvent.emit(newMessage);
+  this.onClear();
+  }
+
+  onClear() {
+    this.subject.nativeElement.value = "";
+    this.msgText.nativeElement.value = "";
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageListComponent implements OnInit {
 
+  messages: Message[] = [
+    new Message("1", "subject", "someText", "someSender"),
+    new Message("2", "subject2", "someText2", "someSender2"),
+    new Message("3", "subject3", "someText3", "someSender3")
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+    console.log(message);
+  }
 }
