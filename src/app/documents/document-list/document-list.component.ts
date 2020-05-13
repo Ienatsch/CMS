5443fe69ad5022a89c766e00081efd92bb393297
dalanienatsch <dienatsch93@gmail.com>
2020-05-13 17:33:28 -0600

    documents page - list and view documents

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter();
 
   constructor() { }
+
+  documents: Document[] = [
+    new Document("1", "Doc1", "Doc1 Description", "Doc1 Url", null),
+    new Document("2", "Doc2", "Doc2 Description", "Doc2 Url", null),
+    new Document("3", "Doc3", "Doc3 Description", "Doc3 Url", null),
+    new Document("4", "Doc4", "Doc4 Description", "Doc4 Url", null),
+    new Document("5", "Doc5", "Doc5 Description", "Doc5 Url", null),
+  ];
 
   ngOnInit(): void {
   }
 
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
